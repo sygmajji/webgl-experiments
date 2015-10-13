0b6533b230c467b97d5dd40b9d803f448f849a6d
sygmajji <sygmajji@gmail.com>
2015-10-12 20:02:23 -0700

    Fix indentation.
    Fix indentation.

--- simple-cube/cube.js
+++ simple-cube/cube.js
@@ -15,51 +15,48 @@ init();
 renderLoop();
 
 function init() {
-
 	// Camera
-    camera = new THREE.PerspectiveCamera( 70, width/height, 1, 1000 );
-    camera.position.y = 150;
-    camera.position.z = 500;
-	
+	camera = new THREE.PerspectiveCamera( 70, width/height, 1, 1000 );
+	camera.position.y = 150;
+	camera.position.z = 500;
+
 	// Scene
-    scene = new THREE.Scene();
+	scene = new THREE.Scene();
 
-    // Cube
-    var geometry = new THREE.BoxGeometry( 200, 200, 200 );
-    for ( var i = 0; i < geometry.faces.length; i += 2 ) {
+	// Cube
+	var geometry = new THREE.BoxGeometry( 200, 200, 200 );
+	for ( var i = 0; i < geometry.faces.length; i += 2 ) {
+		var hex = Math.random() * 0xffffff;
+		geometry.faces[ i ].color.setHex( hex );
+		geometry.faces[ i + 1 ].color.setHex( hex );
+	}
+	var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors, overdraw: 0.5 } );
+	cube = new THREE.Mesh( geometry, material );
+	cube.position.y = 150;
+	scene.add( cube );
 
-        var hex = Math.random() * 0xffffff;
-        geometry.faces[ i ].color.setHex( hex );
-        geometry.faces[ i + 1 ].color.setHex( hex );
-    }
-    var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors, overdraw: 0.5 } );
-    cube = new THREE.Mesh( geometry, material );
-    cube.position.y = 150;
-    scene.add( cube );
-
-    // Plane
-    var geometry = new THREE.PlaneBufferGeometry( 200, 200 );
-    geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
-    var material = new THREE.MeshBasicMaterial( { color: 0xe0e0e0, overdraw: 0.5 } );
-    plane = new THREE.Mesh( geometry, material );
-    scene.add( plane );
+	// Plane
+	var geometry = new THREE.PlaneBufferGeometry( 200, 200 );
+	geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
+	var material = new THREE.MeshBasicMaterial( { color: 0xe0e0e0, overdraw: 0.5 } );
+	plane = new THREE.Mesh( geometry, material );
+	scene.add( plane );
 
 	// Renderer
-    renderer = new THREE.WebGLRenderer( { canvas : myCanvas, antialias : true});
-    renderer.setClearColor( 0xf0f0f0 );
-    renderer.setSize( width, height );
+	renderer = new THREE.WebGLRenderer( { canvas : myCanvas, antialias : true});
+	renderer.setClearColor( 0xf0f0f0 );
+	renderer.setSize( width, height );
 
-    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+	document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 }
 
 function onDocumentMouseDown( event ) {
-	
 	var rect = myCanvas.getBoundingClientRect();
-	
+
 	if ( event.clientX > rect.left &&
-         event.clientX < rect.right && 
-	     event.clientY > rect.top &&
-		 event.clientY < rect.bottom ){
+			event.clientX < rect.right && 
+			event.clientY > rect.top &&
+			event.clientY < rect.bottom ){
 		
 		document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 		document.addEventListener( 'mouseup', onDocumentMouseUp, false );
@@ -72,30 +69,30 @@ function onDocumentMouseDown( event ) {
 }
 
 function onDocumentMouseMove( event ) {
-    mouseX = event.clientX ;
-    cubeRotation = cubeRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
+	mouseX = event.clientX ;
+	cubeRotation = cubeRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
 }
 
 function onDocumentMouseUp( event ) {
-    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-    manual = false;
+	document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+	document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+	manual = false;
 }
 
 function onDocumentMouseOut( event ) {
-    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-    manual = false;
+	document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+	document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+	manual = false;
 }
 
 function renderLoop() {
 	requestAnimationFrame(renderLoop);
-    if (!manual) {
-      cubeRotation += 0.025;
-    }
-    cube.rotation.y = cubeRotation;
-    plane.rotation.y = cube.rotation.y;
-    renderer.render( scene, camera );
+	if (!manual) {
+		cubeRotation += 0.025;
+	}
+	cube.rotation.y = cubeRotation;
+	plane.rotation.y = cube.rotation.y;
+	renderer.render( scene, camera );
 }
